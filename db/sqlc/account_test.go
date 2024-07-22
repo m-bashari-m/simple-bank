fa7546e7b3d12ffee459549e5cae56ec93bf3b7a
m-bashari-m <m-bashari@outlook.com>
2024-07-22 23:46:23 +0330

    feat: implement transaction and tests
    Create a concurrency-safe transaction.
    Write test for updating account balance

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -8,6 +8,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func createRandomAccount(t *testing.T) Account {
+
+	arg := CreateAccountParams{
+		Owner:    utils.RandomOwner(),
+		Balance:  utils.RandomMoney(),
+		Currency: utils.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
 		Owner:    utils.RandomOwner(),
---
+++ db/sqlc/store_test.go
@@ -0,0 +1,106 @@
+package db
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			ctx := context.Background()
+			result, err := store.TransferTx(ctx, TransferTxParams{
+				ToAccountID:   account2.ID,
+				FromAccountID: account1.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	existed := make(map[int]bool)
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transsfer := result.Transfer
+		require.NotEmpty(t, transsfer)
+		require.Equal(t, account1.ID, transsfer.FromAccountID)
+		require.Equal(t, account2.ID, transsfer.ToAccountID)
+		require.Equal(t, amount, transsfer.Amount)
+		require.NotZero(t, transsfer.ID)
+		require.NotZero(t, transsfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transsfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := account2.Balance - toAccount.Balance
+
+		require.Equal(t, diff1, -diff2)
+		require.NotZero(t, diff1)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0)
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
+	}
+
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+
+}
